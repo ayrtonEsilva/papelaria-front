ec2da421409f3565e951c25094377722459d5fed
ayrton <131079261+ayrtonEsilva@users.noreply.github.com>
2023-11-21 21:42:34 -0300

    Commit dia 21/11/23

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import './styles.css'
+import Logo from '../../assets/img/logo2.jpg';
+
+
+
+export default function Dashboard(){
+    return(
+        <div className="logon-container">
+            <h1>Página principal</h1>
+        </div>
+    )
+}
--- src/pages/logon/index.js
+++ src/pages/logon/index.js
@@ -1,10 +1,14 @@
 import './styles.css'
+import Logo from '../../assets/img/logo2.jpg';
 
 
 
 export default function logon(){
     return(
         <div className="logon-container">
+            <div>
+                <img src={Logo} />
+            </div>
             <section className="form">
                 <h1 className="">Faça seu login</h1>
                 <form >
