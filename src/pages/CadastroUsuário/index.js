03597d45d5ebcdb72b506ef61538632181ead2b2
ayrton <131079261+ayrtonEsilva@users.noreply.github.com>
2023-12-01 21:38:20 -0300

    formulario novo usuario

--- src/pages/CadastroUsuário/index.js
+++ src/pages/CadastroUsuário/index.js
@@ -1,5 +1,8 @@
+import React,{useState} from 'react';
 import '../../pages/global.css'
 import{ FiFilePlus }from "react-icons/fi";
+import { FaSave } from "react-icons/fa";
+import { GiCancel } from "react-icons/gi";
 import Menu from '../../componentes/menu';
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import {Link} from 'react-router-dom'
@@ -7,7 +10,7 @@ import Head from '../../componentes/Head';
 
 
 export default function Cadastrousuario(){
-    
+    const [nome, setNome] = useState("");
     return(
         <div className="dashboard-container">
             <div className='menu'>
@@ -15,16 +18,35 @@ export default function Cadastrousuario(){
                 <Menu />
             </div>
             <div className='principal'>
-            <Head title='Cadastro de Usuário'/>
+                <Head title='Cadastro de Usuário'/>
 
-            <form>
-                <input type='text' placeholder='Digite o nome do usuário' />
-                <input type='email' placeholder='Digite o email' />
-                <input type='password' placeholder='Digite a senha' />
-                <button>Salvar</button>
-                <button>Cancelar</button>
-            </form>
-            </div>    
+                <div class="form-container">
+                    <form className='form-cadastro'>
+                        <input type='text'
+                        value={nome}
+                        onChange={e=>setNome(e.target.value)}
+                         placeholder='Digite o nome do usuário' 
+                         />
+                        <input type='email' placeholder='Digite o email' />
+                        <input type='password' placeholder='Digite a senha' />
+                        <div class="acao">
+                            <button className='btn-save'>
+                                <FaSave />
+                                Salvar
+                                </button>
+                            <button className='btn-cancel'>
+                                <GiCancel />
+                                Cancelar
+                    
+                                </button>
+                        </div>
+                    
+                    </form>
+                    
+                </div>    
+            </div>
+
+            
         </div>
     )
 }
