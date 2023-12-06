a802da3f6e6eedffd4eaa19de019f4a1fffcfecc
ayrton <131079261+ayrtonEsilva@users.noreply.github.com>
2023-12-05 21:31:50 -0300

    add local storage

--- src/pages/CadastroUsuário/index.js
+++ src/pages/CadastroUsuário/index.js
@@ -11,6 +11,23 @@ import Head from '../../componentes/Head';
 
 export default function Cadastrousuario(){
     const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const usuario={
+        nome,
+        email,
+        senha
+    }
+
+
+    function salvardados(e){
+        e.preventDefault();
+        // console.log(usuario);
+        const banco = JSON.parse(localStorage.getItem("cd-usuarios")) || "[]";
+        banco.push(usuario);
+        localStorage.setItem("cd-usuarios", JSON.stringify(banco));
+        alert("usuario salvo com sucesso");
+    }
     return(
         <div className="dashboard-container">
             <div className='menu'>
@@ -21,14 +38,24 @@ export default function Cadastrousuario(){
                 <Head title='Cadastro de Usuário'/>
 
                 <div class="form-container">
-                    <form className='form-cadastro'>
+                    <form className='form-cadastro' onSubmit={salvardados}>
                         <input type='text'
                         value={nome}
                         onChange={e=>setNome(e.target.value)}
                          placeholder='Digite o nome do usuário' 
                          />
-                        <input type='email' placeholder='Digite o email' />
-                        <input type='password' placeholder='Digite a senha' />
+
+                        <input type='email' 
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}
+                        placeholder='Digite o email' 
+                        />
+
+                        <input type='password' 
+                        value={senha}
+                        onChange={e=>setSenha(e.target.value)}
+                        placeholder='Digite a senha' 
+                        />
                         <div class="acao">
                             <button className='btn-save'>
                                 <FaSave />
